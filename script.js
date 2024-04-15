f1f18da0947bd21a4d113232030abefea29d5be0
gcopen <164094357+gcopen@users.noreply.github.com>
2024-04-15 18:14:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+
+var output = document.getElementById("date");
+output.innerHTML = "Today's date is: " + dd + "/" + mm + today.getFullYear()+"<br>";tent;
+
+
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+u secondsInAnHour;
+3600
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+secondsInAYear;
+31536000
+
+var age = prompt("How old are you?");
+var age = 29;
+age * secondsInAYear;
